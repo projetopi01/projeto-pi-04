5404c08abeec7b9b38355c82453c370d9796637e
Camila Gonçalves <90690380+CamilaVerso@users.noreply.github.com>
2026-02-24 21:53:02 -0300

    Remove React import from AuthContext.tsx

--- Frontend-projeto-pi-04-main/src/contexts/AuthContext.tsx
+++ Frontend-projeto-pi-04-main/src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 
-import React, { createContext, useState, useContext, type ReactNode, useEffect } from 'react';
+import { createContext, useState, useContext, type ReactNode, useEffect } from 'react';
 import api from '../services/api';
 
 interface AuthContextType {
