a613af6b4cc5076ac406963ed6cfde0aa4eafdf6
ErlandsonS <166462433+projetopi01@users.noreply.github.com>
2026-04-22 21:18:32 -0300

    Update api.ts

--- Frontend-projeto-pi-04-main/src/services/api.ts
+++ Frontend-projeto-pi-04-main/src/services/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // Adicione esta linha
+  baseURL: 'https://projeto-pi-04-c4je.onrender.com', 
+  withCredentials: true,
 });
-
 
 export default api;
