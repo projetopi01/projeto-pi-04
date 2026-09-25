26d482d0061a493402cd2d2dc1fc56954cfc709c
ErlandsonS <166462433+projetopi01@users.noreply.github.com>
2026-09-25 10:34:29 -0300

    Update api.ts

--- Frontend-projeto-pi-04-main/src/services/api.ts
+++ Frontend-projeto-pi-04-main/src/services/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://projeto-pi-04-c4je.onrender.com', 
+  baseURL: '[projeto-pi-04-c4je.onrender.com](https://projeto-pi-04-c4je.onrender.com)', 
   withCredentials: true,
 });
 
 export default api;
+
+export const buscarIndicadores = () => api.get('/api/indicadores');
